02dd465b04ffc8787e0c222866f70ebb1d442c84
kmakmichael <m_kmak@u.pacific.edu>
2021-01-27 19:00:06 -0800

    arm: complete

--- project_1/p1_arm.asm
+++ project_1/p1_arm.asm
@@ -3,7 +3,7 @@ set_1		dcd		10, 20, 10, 20, 20, 501
 set_2		dcd		2, 4, 8, 16, 32, 64, 128, 256, 512
 set_3		dcd		1, 2, 3, 4, 5, 16, 17, 18, 19, 20, 501
 set_4		dcd		501
-
+			
 			
 			;		Register Map:
 			;		r0	current addr
@@ -13,8 +13,18 @@ set_4		dcd		501
 			
 			
 			adr		r0, set_1		; change the dataset here
-
-				
-			
-program_exit
+			mov		r1, #0
+			mov		r2, #0
+			mov		r5, #0
+			bl		count_loop
 			end
+			
+			
+count_loop
+			ldr		r1, [r0], #4
+			cmp		r1, #500
+			movge	r15, r14		; return
+			cmp		r1, #15
+			addlt	r2, r2, #1	; < 15
+			addge	r5, r5, #1	; >= 15
+			b		count_loop
