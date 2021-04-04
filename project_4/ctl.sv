2b4137c68f9d164324c31221a9a9f061a7a2cb07
kmakmichael <m_kmak@u.pacific.edu>
2021-04-04 00:59:28 -0700

    p4: ALUOut for new instructions

--- project_4/ctl.sv
+++ project_4/ctl.sv
@@ -145,9 +145,47 @@ module ctl(
                 6'b001110:  ALUOp <= 5'b10110;  // xori
                 6'b100011:  ALUOp <= 5'b00000;  // lw
                 6'b101011:  ALUOp <= 5'b00000;  // sw
+                6'b000100:  ALUOp <= 5'b00101;  // beq
+                6'b000100:  ALUOp <= 5'b00101;  // bne
                 default:    ALUOp <= 5'b00000;  // nop
             endcase
         end
+    end
+
+    // exceptions
+    always_comb begin
+        case (opCode)
+            6'b001000, // addi
+            6'b001100, // andi
+            6'b001101, // ori
+            6'b001110, // xori
+            6'b100011, // lw
+            6'b101011, // sw
+            6'b000010, // j
+            6'b000011, // jal
+            6'b000100, // beq
+            6'b000101: // bne
+                excp <= 1'b0;
+            6'b000000:
+                case (funct)
+                    6'b100000, // add
+                    6'b100010, // sub
+                    6'b100100, // and
+                    6'b100101, // or
+                    6'b100110, // xor
+                    6'b100111, // nor
+                    6'b101010, // slt
+                    6'b000000, // sll
+                    6'b000010, // srl
+                    6'b000011, // sra
+                    6'b001001: // jr
+                        excp <= 1'b0;
+                    default:
+                        excp <= 1'b1;
+                endcase
+            default:
+                excp <= 1'b1;
+        endcase
     end
 
 endmodule
